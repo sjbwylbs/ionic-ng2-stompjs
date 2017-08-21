3586beee4ebbd7c5342b73ef4d381c64990bbab9
花米 <sjbwylbs@163.com>
2017-08-22 01:10:34 +0800

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,37 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import * as SockJS from 'sockjs-client';
+import {StompConfig, StompService} from '@stomp/ng2-stompjs';
+
+export function socketProvider() {
+  return new SockJS('ws://127.0.0.1:15763');
+}
+
+const stompConfig: StompConfig = {
+  // Which server?
+  url: socketProvider,
+
+  // Headers
+  // Typical keys: login, passcode, host
+  headers: {
+    login: 'guest',
+    passcode: 'guest'
+  },
+
+  // How often to heartbeat?
+  // Interval in milliseconds, set to 0 to disable
+  heartbeat_in: 0, // Typical value 0 - disabled
+  heartbeat_out: 20000, // Typical value 20000 - every 20 seconds
+
+  // Wait in milliseconds before attempting auto reconnect
+  // Set to 0 to disable
+  // Typical value 5000 (5 seconds)
+  reconnect_delay: 5000,
+
+  // Will log diagnostics on console
+  debug: true
+};
 
 @NgModule({
   declarations: [
@@ -24,6 +55,8 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
+    StompService,
+    {provide: StompConfig, useValue: stompConfig},
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
